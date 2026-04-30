ad0afcaf278ac75b21504f2554b1cbaa463b27c8
User Kovalyov <dan.cowaleff@yandex.ru>
2026-04-30 15:28:39 +0300

    Каркас приложения

--- src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
@@ -1,8 +1,29 @@
 package ru.yandex.practicum.sleeptracker;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
 public class SleepTrackerApp {
+    LogsLoader logsLoader = new LogsLoader();
+    String pathFile = "src/main/resources/sleep_log.txt";
+    List<SleepingSession> sleepingSessions;
+    {
+        try {
+            sleepingSessions = logsLoader.downloadSession(pathFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите путь до файла Log.txt:");
+        /*
+        String pathFile = scanner.nextLine();
+        SleepTrackerApp app = new SleepTrackerApp();
+         */
+
 
     }
 }
