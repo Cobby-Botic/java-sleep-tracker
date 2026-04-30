5503afa55e496d1ef7c5bb665371b9e3e785d776
Daniel <dan.cowaleff@yandex.ru>
2026-04-30 18:27:19 +0300

    Общие доработки main-класса

--- src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/SleepTrackerApp.java
@@ -1,29 +1,27 @@
 package ru.yandex.practicum.sleeptracker;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class SleepTrackerApp {
-    LogsLoader logsLoader = new LogsLoader();
-    String pathFile = "src/main/resources/sleep_log.txt";
-    List<SleepingSession> sleepingSessions;
-    {
-        try {
-            sleepingSessions = logsLoader.downloadSession(pathFile);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+        final LogsLoader logsLoader = new LogsLoader();
+        List<SleepingSession> sleepingSessions;
+
         System.out.println("Введите путь до файла Log.txt:");
-        /*
-        String pathFile = scanner.nextLine();
-        SleepTrackerApp app = new SleepTrackerApp();
-         */
-
-
+        String pathFile = "src/main/resources/sleep_lg.txt";
+        try {
+            sleepingSessions = logsLoader.downloadSession(pathFile);
+            List<SleepAnalizator> analyzers = new ArrayList<>();
+            analyzers.add(new CountSessionSleeps());
+            analyzers.forEach((analyzer) -> System.out.println(analyzer.analyze(sleepingSessions)));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
